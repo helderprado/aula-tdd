b9bb63203f96de67469533410a97fa454c749c8c
Helder <helderprado@gmail.com>
2024-11-04 11:37:42 -0300

    ajustes realizados

--- referencia/src/tests/usecases/integration/test_find_user.py
+++ referencia/src/tests/usecases/integration/test_find_user.py
@@ -1,18 +1,34 @@
-from uuid import uuid4
-from domain.user.user_entity import User
+from uuid import UUID, uuid4
+from application.user.find_user import FindUser, FindUserRequest
 from infra.user.in_memory_user_repository import InMemoryUserRepository
-from application.user.find_user import FindUser, FindUserRequest, FindUserResponse
+from domain.user.user_entity import User
 
 
 class TestFindUser:
 
-    def test_find_user_with_valid_data(self):
+    def test_find_user_by_id(self):
+        # Cria um repositório de usuários em memória
+        repository = InMemoryUserRepository()
 
-        user1 = User(id=uuid4(), name="João")
-        user2 = User(id=uuid4(), name="Maria")
-        repository = InMemoryUserRepository(users=[user1, user2])
+        # Cria e adiciona um usuário ao repositório
+        user_id = uuid4()
+        user = User(id=user_id, name="João")
+        repository.save(user)
+
+        # Instancia o caso de uso de busca de usuário, passando o repositório como dependência
         use_case = FindUser(repository=repository)
-        request = FindUserRequest(id=user2.id)
+
+        # Cria uma requisição de busca de usuário pelo ID
+        request = FindUserRequest(id=user_id)
+
+        # Executa o caso de uso com a requisição e armazena a resposta
         response = use_case.execute(request)
 
-        assert response == FindUserResponse(id=user2.id, name=user2.name)
+        # Verifica se a resposta não é nula, indicando que o usuário foi encontrado
+        assert response is not None
+        # Verifica se o ID do usuário encontrado é uma instância válida de UUID
+        assert isinstance(response.id, UUID)
+        # Verifica se o ID do usuário encontrado corresponde ao ID buscado
+        assert response.id == user_id
+        # Verifica se o nome do usuário encontrado é o mesmo que o nome do usuário salvo
+        assert response.name == "João"
